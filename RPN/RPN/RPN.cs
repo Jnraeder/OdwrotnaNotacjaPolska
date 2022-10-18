fea31742181313305d2d5e95ff8c29d964ac0798
Jnraeder <46370319+Jnraeder@users.noreply.github.com>
2022-10-18 11:15:28 +0200

    Wprowadzenie rozwiązanego zadania
    Wprowadzenie rozwiązanego zadania po dokładnym jak działa.
    Split rozdziela dany string i używając różnych case'ów sprawdza jaki ma być system liczbowy i jaka operacja.
    Jest oczywiście więcej szczegółów jak dictionary stack itd, i mogę to opisać słownie.

--- RPN/RPN/RPN.cs
+++ RPN/RPN/RPN.cs
@@ -12,7 +12,12 @@ namespace RPNCalulator {
 				["+"] = (fst, snd) => (fst + snd),
 				["-"] = (fst, snd) => (fst - snd),
 				["*"] = (fst, snd) => (fst * snd),
-				["/"] = (fst, snd) => (fst / snd)
+				["/"] = (fst, snd) => (fst / snd),
+				["!"] = (fst, snd) =>
+				{
+					int v = Factorial(snd);
+					return v;
+				}
 			};
 			_operators = new Stack<int>();
 
@@ -20,7 +25,70 @@ namespace RPNCalulator {
 			foreach (var op in splitInput)
 			{
 				if (IsNumber(op))
-					_operators.Push(Int32.Parse(op));
+				{
+					if (op.Substring(0, 1) == "D")
+					{
+						_operators.Push(Int32.Parse(op.Substring(1, op.Length-1)));
+					}
+					if (op.Substring(0, 1) == "B")
+					{
+						int number = 0, c = (int)Math.Pow(2,op.Length - 2);
+						foreach (char digit in op.Substring(1, op.Length-1))
+						{
+							switch (digit)
+							{
+								case '0':
+								case '1':
+									number += Int32.Parse(digit + "") * c;
+									break;
+							}
+							c /= 2;
+						}
+						_operators.Push(number);
+					}
+					if (op.Substring(0, 1) == "#")
+					{
+						int number = 0, c = (int)Math.Pow(16, op.Length - 2);
+						foreach (var digit in op.Substring(1, op.Length - 1))
+						{
+							switch (digit)
+							{
+								case '0':
+								case '1':
+								case '2':
+								case '3':
+								case '4':
+								case '5':
+								case '6':
+								case '7':
+								case '8':
+								case '9':
+									number += Int32.Parse(digit + "") * c;
+									break;
+								case 'A':
+									number += 10 * c;
+									break;
+								case 'B':
+									number += 11 * c;
+									break;
+								case 'C':
+									number += 12 * c;
+									break;
+								case 'D':
+									number += 13 * c;
+									break;
+								case 'E':
+									number += 14 * c;
+									break;
+								case 'F':
+									number += 15 * c;
+									break;
+							}
+							c /= 16;
+						}
+						_operators.Push(number);
+					}
+				}
 				else
 				if (IsOperator(op))
 				{
@@ -28,6 +96,12 @@ namespace RPNCalulator {
 					var num2 = _operators.Pop();
 					_operators.Push(_operationFunction[op](num1, num2));
 					//_operators.Push(Operation(op)(num1, num2));
+				}
+				else
+				if (IsOperatorUnary(op))
+				{
+					var num1 = _operators.Pop();
+					_operators.Push(_operationFunction[op](0, num1));
 				}
 			}
 
@@ -38,12 +112,88 @@ namespace RPNCalulator {
 			}
 			throw new InvalidOperationException();
 		}
+		private int Factorial(int f)
+		{
+			int fact = 1;
+			for (int i = 1; i <= f; i++)
+			{
+				fact = fact * i;
+			}
+			return fact;
+		}
 
-		private bool IsNumber(String input) => Int32.TryParse(input, out _);
+		private bool IsNumber(String input) {
+			if (input.Substring(0, 1) == "B") {
+				foreach (var digit in input.Substring(1, input.Length - 1))
+				{
+					switch (digit)
+					{
+						case '0':
+						case '1':
+							return true;
+						default:
+							return false;
+					}
+				}
+			}
+			else if(input.Substring(0, 1) ==  "D")
+			{
+				foreach (var digit in input.Substring(1, input.Length - 1)) {
+					switch (digit)
+					{
+						case '0':
+						case '1':
+						case '2':
+						case '3':
+						case '4':
+						case '5':
+						case '6':
+						case '7':
+						case '8':
+						case '9':
+							return true;
+						default:
+							return false;
+					}
+				}
+			}
+			else if(input.Substring(0, 1) == "#")
+			{
+				foreach (var digit in input.Substring(1, input.Length - 1))
+				{
+					switch (digit)
+					{
+						case '0':
+						case '1':
+						case '2':
+						case '3':
+						case '4':
+						case '5':
+						case '6':
+						case '7':
+						case '8':
+						case '9':
+						case 'A':
+						case 'B':
+						case 'C':
+						case 'D':
+						case 'E':
+						case 'F':
+							return true;
+						default:
+							return false;
+					}
+				}
+			}
+			return false;
+		}
 
 		private bool IsOperator(String input) =>
 			input.Equals("+") || input.Equals("-") ||
 			input.Equals("*") || input.Equals("/");
+
+		private bool IsOperatorUnary(String input) =>
+			input.Equals("!");
 
 		private Func<int, int, int> Operation(String input) =>
 			(x, y) =>
@@ -52,5 +202,6 @@ namespace RPNCalulator {
 					(input.Equals("*") ? x * y : int.MinValue)
 				)
 			);
+
 	}
 }
--- RPNTest/RPNTest.cs
+++ RPNTest/RPNTest.cs
@@ -10,6 +10,11 @@ namespace RPNTest {
 		public void Setup() {
 			_sut = new RPN();
 		}
+
+
+		//TESTY OGÓLNE
+
+
 		[Test]
 		public void CheckIfTestWorks() {
 			Assert.Pass();
@@ -21,54 +26,335 @@ namespace RPNTest {
 		}
 
 		[Test]
-		public void SingleDigitOneInputOneReturn() {
-			var result = _sut.EvalRPN("1");
+		public void WrongDigit_ThrowsException()
+		{
+			Assert.Throws<InvalidOperationException>(() => _sut.EvalRPN("Q2"));
+		}
+
+
+		//TESTY BINARNE
+
+
+		[Test]
+		public void Binary_WrongDigit_ThrowsException()
+		{
+			Assert.Throws<InvalidOperationException>(() => _sut.EvalRPN("B2"));
+		}
+
+		[Test]
+		public void Binary_SingleDigitOneInputOneReturn()
+		{
+			var result = _sut.EvalRPN("B1");
 
 			Assert.That(result, Is.EqualTo(1));
-
 		}
+
 		[Test]
-		public void SingleDigitOtherThenOneInputNumberReturn() {
-			var result = _sut.EvalRPN("2");
+		public void Binary_TwoDigitsNumberInputNumberReturn()
+		{
+			var result = _sut.EvalRPN("B10");
 
 			Assert.That(result, Is.EqualTo(2));
-
 		}
+
 		[Test]
-		public void TwoDigitsNumberInputNumberReturn() {
-			var result = _sut.EvalRPN("12");
-
-			Assert.That(result, Is.EqualTo(12));
-
+		public void Binary_TwoNumbersGivenWithoutOperator_ThrowsExcepton()
+		{
+			Assert.Throws<InvalidOperationException>(() => _sut.EvalRPN("B1 B10"));
 		}
-		[Test]
-		public void TwoNumbersGivenWithoutOperator_ThrowsExcepton() {
-			Assert.Throws<InvalidOperationException>(() => _sut.EvalRPN("1 2"));
 
-		}
 		[Test]
-		public void OperatorPlus_AddingTwoNumbers_ReturnCorrectResult() {
-			var result = _sut.EvalRPN("1 2 +");
+		public void Binary_OperatorPlus_AddingTwoNumbers_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("B1 B10 +");
 
 			Assert.That(result, Is.EqualTo(3));
 		}
+
 		[Test]
-		public void OperatorTimes_AddingTwoNumbers_ReturnCorrectResult() {
-			var result = _sut.EvalRPN("2 2 *");
+		public void Binary_OperatorTimes_AddingTwoNumbers_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("B10 B10 *");
 
 			Assert.That(result, Is.EqualTo(4));
 		}
+
 		[Test]
-		public void OperatorMinus_SubstractingTwoNumbers_ReturnCorrectResult() {
-			var result = _sut.EvalRPN("1 2 -");
+		public void Binary_OperatorMinus_SubstractingTwoNumbers_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("B1 B10 -");
 
 			Assert.That(result, Is.EqualTo(1));
 		}
+
 		[Test]
-		public void ComplexExpression() {
-			var result = _sut.EvalRPN("15 7 1 1 + - / 3 * 2 1 1 + + -");
+		public void Binary_OperatorSlash_SubstractingTwoNumbers_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("B10 B101 /");
+
+			Assert.That(result, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void Binary_OperatorSlash_DividingByZero_ThrowException()
+		{
+			Assert.Throws<DivideByZeroException>(() => _sut.EvalRPN("B0 B1110 /"));
+		}
+
+		[Test]
+		public void Binary_OperatorDivision_DividerBiggerThanDividend_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("B110 B10 /");
+
+			Assert.That(result, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void Binary_OperatorSilnia_CalculatingTwoNumbers_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("B11 !");
+
+			Assert.That(result, Is.EqualTo(6));
+		}
+
+		[Test]
+		public void Binary_OperatorSilnia_CalculatingZero_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("B0 !");
+
+			Assert.That(result, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void Binary_OperatorSilnia_CalculatingTwoNumbers_ThrowsException()
+		{
+			Assert.Throws<InvalidOperationException>(() => _sut.EvalRPN("B11 B110 !"));
+		}
+
+		[Test]
+		public void Binary_ComplexExpression()
+		{
+			var result = _sut.EvalRPN("B1111 B111 B1 B1 + - / B11 * B10 B1 B1 + + -");
 
 			Assert.That(result, Is.EqualTo(4));
 		}
+
+
+		//TESTY DZIESIĘTNE
+
+
+		[Test]
+		public void Decimal_WrongDigit_ThrowsException()
+		{
+			Assert.Throws<InvalidOperationException>(() => _sut.EvalRPN("DG"));
+		}
+
+		[Test]
+		public void Decimal_SingleDigitOneInputOneReturn() {
+			var result = _sut.EvalRPN("D1");
+
+			Assert.That(result, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void Decimal_SingleDigitOtherThenOneInputNumberReturn() {
+			var result = _sut.EvalRPN("D2");
+
+			Assert.That(result, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void Decimal_TwoDigitsNumberInputNumberReturn() {
+			var result = _sut.EvalRPN("D12");
+
+			Assert.That(result, Is.EqualTo(12));
+		}
+
+		[Test]
+		public void Decimal_TwoNumbersGivenWithoutOperator_ThrowsExcepton() {
+			Assert.Throws<InvalidOperationException>(() => _sut.EvalRPN("D1 D2"));
+		}
+
+		[Test]
+		public void Decimal_OperatorPlus_AddingTwoNumbers_ReturnCorrectResult() {
+			var result = _sut.EvalRPN("D1 D2 +");
+
+			Assert.That(result, Is.EqualTo(3));
+		}
+
+		[Test]
+		public void Decimal_OperatorTimes_AddingTwoNumbers_ReturnCorrectResult() {
+			var result = _sut.EvalRPN("D2 D2 *");
+
+			Assert.That(result, Is.EqualTo(4));
+		}
+
+		[Test]
+		public void Decimal_OperatorMinus_SubstractingTwoNumbers_ReturnCorrectResult() {
+			var result = _sut.EvalRPN("D1 D2 -");
+
+			Assert.That(result, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void Decimal_OperatorSlash_SubstractingTwoNumbers_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("D1 D2 /");
+
+			Assert.That(result, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void Decimal_OperatorSlash_DividingByZero_ThrowException()
+		{
+			Assert.Throws<DivideByZeroException>(() => _sut.EvalRPN("D0 D14 /"));
+		}
+
+		[Test]
+		public void Decimal_OperatorDivision_DividerBiggerThanDividend_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("D6 D2 /");
+
+			Assert.That(result, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void Decimal_OperatorSilnia_CalculatingTwoNumbers_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("D3 !");
+
+			Assert.That(result, Is.EqualTo(6));
+		}
+
+		[Test]
+		public void Decimal_OperatorSilnia_CalculatingZero_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("D0 !");
+
+			Assert.That(result, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void Decimal_OperatorSilnia_CalculatingTwoNumbers_ThrowsException()
+		{
+			Assert.Throws<InvalidOperationException>(() => _sut.EvalRPN("D3 D6 !"));
+		}
+
+		[Test]
+		public void Decimal_ComplexExpression() {
+			var result = _sut.EvalRPN("D15 D7 D1 D1 + - / D3 * D2 D1 D1 + + -");
+
+			Assert.That(result, Is.EqualTo(4));
+		}
+
+
+		//TESTY SZESNASTKOWE
+
+
+		[Test]
+		public void HexaDecimal_WrongDigit_ThrowsException()
+		{
+			Assert.Throws<InvalidOperationException>(() => _sut.EvalRPN("#W"));
+		}
+
+		[Test]
+		public void HexaDecimal_SingleDigitOneInputOneReturn()
+		{
+			var result = _sut.EvalRPN("#C");
+
+			Assert.That(result, Is.EqualTo(12));
+		}
+
+		[Test]
+		public void HexaDecimal_TwoDigitsNumberInputNumberReturn()
+		{
+			var result = _sut.EvalRPN("#11");
+
+			Assert.That(result, Is.EqualTo(17));
+		}
+
+		[Test]
+		public void HexaDecimal_TwoNumbersGivenWithoutOperator_ThrowsExcepton()
+		{
+			Assert.Throws<InvalidOperationException>(() => _sut.EvalRPN("#C #F"));
+		}
+
+		[Test]
+		public void HexaDecimal_OperatorPlus_AddingTwoNumbers_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("#7 #D +");
+
+			Assert.That(result, Is.EqualTo(20));
+		}
+
+		[Test]
+		public void HexaDecimal_OperatorTimes_AddingTwoNumbers_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("#2 #C *");
+
+			Assert.That(result, Is.EqualTo(24));
+		}
+
+		[Test]
+		public void HexaDecimal_OperatorMinus_SubstractingTwoNumbers_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("#1 #A -");
+
+			Assert.That(result, Is.EqualTo(9));
+		}
+
+		[Test]
+		public void HexaDecimal_OperatorSlash_SubstractingTwoNumbers_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("#B #16 /");
+
+			Assert.That(result, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void HexaDecimal_OperatorSlash_DividingByZero_ThrowException()
+		{
+			Assert.Throws<DivideByZeroException>(() => _sut.EvalRPN("#0 #F /"));
+		}
+
+		[Test]
+		public void HexaDecimal_OperatorDivision_DividerBiggerThanDividend_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("#F #E /");
+
+			Assert.That(result, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void HexaDecimal_OperatorSilnia_CalculatingTwoNumbers_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("#5 !");
+
+			Assert.That(result, Is.EqualTo(120));
+		}
+
+		[Test]
+		public void HexaDecimal_OperatorSilnia_CalculatingZero_ReturnCorrectResult()
+		{
+			var result = _sut.EvalRPN("#0 !");
+
+			Assert.That(result, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void HexaDecimal_OperatorSilnia_CalculatingTwoNumbers_ThrowsException()
+		{
+			Assert.Throws<InvalidOperationException>(() => _sut.EvalRPN("@B @A0 !"));
+		}
+
+		[Test]
+		public void HexaDecimal_ComplexExpression()
+		{
+			var result = _sut.EvalRPN("#F #7 #1 #1 + - / #3 * #2 #1 #1 + + -");
+
+			Assert.That(result, Is.EqualTo(4));
+		}
+
+
 	}
 }
